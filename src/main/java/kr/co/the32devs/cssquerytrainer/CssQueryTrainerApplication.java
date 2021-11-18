ae0a99557c701edb240726d84e67c328f8426f32
jparangdev <jparangdev@gmail.com>
2021-11-18 17:49:12 +0900

    Initial commit

---
+++ src/main/java/kr/co/the32devs/cssquerytrainer/CssQueryTrainerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.the32devs.cssquerytrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssQueryTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssQueryTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/the32devs/cssquerytrainer/CssQueryTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.the32devs.cssquerytrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssQueryTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
